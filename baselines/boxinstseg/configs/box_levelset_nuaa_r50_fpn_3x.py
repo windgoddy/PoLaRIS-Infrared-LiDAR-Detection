c95c754182ae4f80729b9cad52e226f7d690022e
zhangxizhe <zxz0924@stud.tjut.edu.cn>
2026-04-10 15:52:56 +0800

    OOM

--- baselines/boxinstseg/configs/box_levelset_nuaa_r50_fpn_3x.py
+++ baselines/boxinstseg/configs/box_levelset_nuaa_r50_fpn_3x.py
@@ -35,9 +35,9 @@ model = dict(
         stacked_convs=4,
         seg_feat_channels=256,
         strides=[8, 8, 16, 32, 32],
-        scale_ranges=((1, 96), (48, 192), (96, 384), (192, 768), (384, 2048)),
+        scale_ranges=((1, 32), (16, 64), (32, 128), (64, 256), (128, 2048)),
         sigma=0.2,
-        num_grids=[40, 36, 24, 16, 12],
+        num_grids=[16, 12, 10, 8, 6],
         cate_down_pos=0,
         loss_cate=dict(
             type="FocalLoss",
@@ -68,7 +68,7 @@ train_pipeline = [
     dict(type="LoadImageFromFile"),
     dict(type="LoadAnnotations", with_bbox=True, with_mask=False),
     dict(type="GenerateBoxMask"),
-    dict(type="Resize", img_scale=(1333, 640), keep_ratio=True),
+    dict(type="Resize", img_scale=(256, 256), keep_ratio=True),
     dict(type="RandomFlip", flip_ratio=0.5),
     dict(type="Normalize", **img_norm_cfg),
     dict(type="Pad", size_divisor=32),
@@ -79,7 +79,7 @@ test_pipeline = [
     dict(type="LoadImageFromFile"),
     dict(
         type="MultiScaleFlipAug",
-        img_scale=(1333, 640),
+        img_scale=(256, 256),
         flip=False,
         transforms=[
             dict(type="Resize", keep_ratio=True),
