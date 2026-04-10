1da520e1018107beef410a507c9a0d19f578110f
zhangxizhe <zxz0924@stud.tjut.edu.cn>
2026-04-10 17:01:16 +0800

    OOM

--- baselines/boxinstseg/configs/box_levelset_irstd1k_r50_fpn_3x.py
+++ baselines/boxinstseg/configs/box_levelset_irstd1k_r50_fpn_3x.py
@@ -68,7 +68,7 @@ train_pipeline = [
     dict(type="LoadImageFromFile"),
     dict(type="LoadAnnotations", with_bbox=True, with_mask=False),
     dict(type="GenerateBoxMask"),
-    dict(type="Resize", img_scale=(512, 512), keep_ratio=True),
+    dict(type="Resize", img_scale=(256, 256), keep_ratio=True),
     dict(type="RandomFlip", flip_ratio=0.5),
     dict(type="Normalize", **img_norm_cfg),
     dict(type="Pad", size_divisor=32),
@@ -79,7 +79,7 @@ test_pipeline = [
     dict(type="LoadImageFromFile"),
     dict(
         type="MultiScaleFlipAug",
-        img_scale=(512, 512),
+        img_scale=(256, 256),
         flip=False,
         transforms=[
             dict(type="Resize", keep_ratio=True),
--- baselines/boxinstseg/work_dirs/box_levelset_nuaa_r50_fpn_3x/box_levelset_nuaa_r50_fpn_3x.py
+++ baselines/boxinstseg/work_dirs/box_levelset_nuaa_r50_fpn_3x/box_levelset_nuaa_r50_fpn_3x.py
@@ -18,6 +18,7 @@ model = dict(
     neck=dict(
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
+        
         out_channels=256,
         start_level=0,
         num_outs=5),
